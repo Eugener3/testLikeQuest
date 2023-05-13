8066bdd4ae305b5809bcd56f322e9f70ff4f92dd
Eugener3 <musi-masi@mail.ru>
2023-05-14 01:12:08 +0700

    added categories

--- AdminPanel.xaml.cs
+++ AdminPanel.xaml.cs
@@ -23,13 +23,48 @@ namespace testLikeQuest
         public AdminPanel()
         {
             InitializeComponent();
+            FillComboBox();
+            FillSelComboBox();
         }
+
 
         private void AddQuestionButton_Click(object sender, RoutedEventArgs e)
         {
             ButtonsPanel.Visibility = Visibility.Hidden;
             addQuestionPanel.Visibility = Visibility.Visible;
             BackButton.Visibility = Visibility.Visible;
+        }
+
+        private void FillComboBox()
+        {
+            categoryBox.Items.Clear();
+
+            tlqEntities db = new tlqEntities();
+
+            using (db)
+            {
+                foreach (categories c in db.categories)
+                {
+
+                    categoryBox.Items.Add(c.nameCategory); 
+                }
+            }
+        }
+
+        private void FillSelComboBox()
+        {
+            SelcategoryBox.Items.Clear();
+
+            tlqEntities db = new tlqEntities();
+
+            using (db)
+            {
+                foreach (categories c in db.categories)
+                {
+
+                    SelcategoryBox.Items.Add(c.nameCategory);
+                }
+            }
         }
 
         private void CategoryButton_Click(object sender, RoutedEventArgs e)
@@ -40,11 +75,41 @@ namespace testLikeQuest
         }
         private void AddCategoryButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
+            tlqEntities db = new tlqEntities();
+
+            using (db)
+            {
+                categories category = new categories();
+                if (categoryBox.Text != null)
+                {
+                    int maxIdCategory = (from us in db.categories select us.idCategory).Max();
+
+                    category.nameCategory = categoryBox.Text;
+                    category.idCategory = maxIdCategory + 1;
+
+                    db.categories.Add(category);
+                    db.SaveChanges();
+                    MessageBox.Show("Категория успешно создана");
+                    categoryBox.Text = null;
+                    FillComboBox();
+                }
+            }
         }
         private void DeleteCategoryButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            tlqEntities db = new tlqEntities();
+
+            using (db)
+            {
+                // Разобраться с синтактисом удаления категории
+
+                MessageBox.Show("Категория успешно удалена");
+                categoryBox.Text = null;
+                FillComboBox();
+            }
+
+                
         }
 
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
